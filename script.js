2ec9ecdf8b166459589349a686a6033396d38407
Clay Gibson <blackcreekproductions@gmail.com>
2022-07-15 13:23:59 -0400

    initial working slider

--- script.js
+++ script.js
@@ -12,42 +12,10 @@ const btnForward = document.querySelector(
 );
 const btnBack = document.querySelector('.btn-slider-back');
 
-const posterImgs = document.querySelectorAll('.poster-img');
-
 //Generic function for closing an element arg is the element to be closed
 const toggleElement = function (element) {
 	element.classList.toggle('closed');
 };
-
-//add an event listener to each of the posters
-posterImgs.forEach((poster) => {
-	poster.addEventListener('click', function (e) {
-		//toggle open or closed when clicked
-		e.target
-			.closest('.projects-poster')
-			.classList.toggle('projects-clicked');
-		//centers the poster in the view
-		document
-			.querySelector('.projects-content')
-			.scrollIntoView(false);
-	});
-});
-
-document
-	.querySelectorAll('.poster-close')
-	.forEach((element) => {
-		element.addEventListener('click', function (e) {
-			document
-				.querySelectorAll('.projects-clicked')
-				.forEach((element) => {
-					element.classList.remove('projects-clicked');
-					console.log('removing');
-				});
-			document
-				.querySelector('.projects-poster')
-				.scrollIntorView(false);
-		});
-	});
 
 //function for scroll over
 function highlight(el) {
@@ -99,40 +67,192 @@ document
 			.querySelector('.about-subheading')
 			.scrollIntoView(true);
 	});
+
+const changeSlide = function (direction) {
+	//guard clause if at the end of the loop
+	//Needs to move the whole UL left or right 1 space
+	// divide by length to get how much to move
+};
+
+let curSlide = 0;
+
+////Move the poster Slided/////
+// const changeSlide = function (dir) {
+// 	if (dir === 'forwards') {
+// 		if (curSlide < 2) {
+// 			curSlide++;
+// 			console.log(curSlide);
+// 		}
+// 	}
+// 	if (dir === 'back') {
+// 		if (curSlide > -2) {
+// 			curSlide--;
+// 			console.log(curSlide);
+// 		}
+// 	}
+// 	projectsContent.style.transform = `translateX(${
+// 		curSlide * -33.33
+// 	}%)`;
+// };
+
+let counter = 1;
+
+//current and next slide
+let imgCurrent = null;
+let imgNext = null;
+
+//make an array of objects with given info
+const posters = [
+	{
+		src: './img/niles-norman-poster.png',
+		text: 'Here is some text',
+		title: 'Here is a title',
+		subImgs: [
+			'./img/mountains-concept.png',
+			'./img/sealpup-sketch.PNG',
+		],
+	},
+	{
+		src: './img/SplashPoster.jpg',
+		text: 'learn to swim',
+		title: 'Splash! (2D Animated Short Film)',
+		subImgs: [
+			'./img/mountains-concept.png',
+			'./img/sealpup-sketch.PNG',
+		],
+	},
+	{
+		src: './img/quest-of-heros-poster.jpg',
+		text: 'Here is some text',
+		title: 'Here is a title',
+		subImgs: [
+			'./img/mountains-concept.png',
+			'./img/sealpup-sketch.PNG',
+		],
+	},
+	{
+		src: './img/quest-of-heros-poster.jpg',
+		text: 'Here is some text',
+		title: 'Here is a title',
+		subImgs: [
+			'./img/mountains-concept.png',
+			'./img/sealpup-sketch.PNG',
+		],
+	},
+];
+
+//Creates an img html markup for each image in the list
+const projectsImgs = function (imgs) {
+	let markup = ``;
+	imgs.forEach((img) => {
+		markup += `<img
+				class="project-img"
+				src="${img}"
+			/>`;
+	});
+	return markup;
+};
+
+const createImg = function (index) {
+	const newItem = `<li class="projects-poster" data-id=${index}>
+				<div class="poster-wide "> 
+			
+			 <img src="${
+					posters[index].src
+				}" class="poster-img shadow" /> 
+		
+			<aside class="poster-content">
+						<div class="poster-close">
+							<ion-icon name="close-outline"></ion-icon>
+						</div>
+						<h2 class="poster-heading subheading">${
+							posters[index].title
+						}</h2>
+						<p class="poster-paragraph paragraph"> ${
+							posters[index].text
+						} </p>
+						<div class="project-imgs">
+							${projectsImgs(posters[index].subImgs)}
+
+							
+							
+						</div>
+					</aside>
+				</div>
+			</li> `;
+	//Add the image in
+
+	document
+		.querySelector('.projects-content')
+		.insertAdjacentHTML('beforeend', newItem);
+};
+
+//Initalize posters
+posters.forEach((poster, index) => {
+	createImg(index);
+});
+
+const movePosters = function (id, direction) {
+	let distance = -33;
+	if (direction === 'right') {
+		distance = 0;
+	}
+	projectsContent.style.transform = `translateX(${
+		+id * distance
+	}%)`;
+};
+
+const togglePosters = function (clicked) {
+	//decide if opening or closing an element
+	//opening push left closing push right
+	let direction = 'left';
+	if (clicked.classList.contains('projects-clicked')) {
+		direction = 'right';
+	}
+	clicked.classList.toggle('projects-clicked');
+	//Needs to know the order and which way to move
+	movePosters(clicked.dataset.id, direction);
+
+	//centers the poster in the view
+	document
+		.querySelector('.projects-content')
+		.scrollIntoView(false);
+};
+
+const posterImgs = document.querySelectorAll('.poster-img');
+//add an event listener to each of the posters
+posterImgs.forEach((poster) => {
+	poster.addEventListener('click', function (e) {
+		//toggle open or closed when clicked
+		let clicked = e.target.closest('.projects-poster');
+		togglePosters(clicked);
+	});
+});
+
+document
+	.querySelectorAll('.poster-close')
+	.forEach((element) => {
+		element.addEventListener('click', function (e) {
+			let clicked = e.target.closest('.projects-poster');
+			togglePosters(clicked);
+		});
+	});
+
+// KEEEP THIS
 ///Listen for clicks on the slider arrows
 const sliderForwardListener = function () {
 	btnForward.addEventListener('click', function () {
-		changeSlide('forwards');
+		movePosters(1, 'left');
 	});
 };
 const sliderBackListener = function () {
 	btnBack.addEventListener('click', function () {
-		changeSlide('back');
+		changeSlide(1, 'right');
 	});
 };
 
 sliderForwardListener();
 sliderBackListener();
-let curSlide = 0;
-
-////Move the poster Slided/////
-const changeSlide = function (dir) {
-	if (dir === 'forwards') {
-		if (curSlide < 2) {
-			curSlide++;
-			console.log(curSlide);
-		}
-	}
-	if (dir === 'back') {
-		if (curSlide > -2) {
-			curSlide--;
-			console.log(curSlide);
-		}
-	}
-	projectsContent.style.transform = `translateX(${
-		curSlide * -33.33
-	}%)`;
-};
 
 const clickListener = function (clickEl, closeEl) {
 	clickEl.addEventListener('click', function () {
